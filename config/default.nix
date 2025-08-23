f22d3bcc80b1e660e4bfb744b1527360c7263c52
theurgi <58859663+theurgi@users.noreply.github.com>
2025-08-23 18:42:53 -0400

    configure vim.options

--- config/default.nix
+++ config/default.nix
@@ -1,3 +1,21 @@
 {
-  vim = {};
+  vim = {
+    options = {
+      autoindent = true;
+      cursorlineopt = "both";
+      expandtab = true;
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+      signcolumn = "yes";
+      smartindent = true;
+      splitbelow = true;
+      splitright = true;
+      tabstop = 2;
+      termguicolors = true;
+      timeoutlen = 500;
+      updatetime = 250;
+      wrap = false;
+    };
+  };
 }
